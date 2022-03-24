ff50832f2bf78d6b17727d93810acb4745aef0c5
kimdoa <ehdk1409@humuson.com>
2022-03-24 17:20:06 +0900

    feat: 프론트 추가(vue 사용)

--- src/main/java/com/example/spring_project/SpringProjectApplication.java
+++ src/main/java/com/example/spring_project/SpringProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}, scanBasePackages = "com.example.spring_project")
 public class SpringProjectApplication {
 
     public static void main(String[] args) {
