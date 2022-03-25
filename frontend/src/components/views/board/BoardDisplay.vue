a3dc9f4399b76ed1cbb65b9b08d72323466cd34f
kimdoa <ehdk1409@humuson.com>
2022-03-25 16:29:27 +0900

    문구 수정

--- frontend/src/components/views/board/BoardDisplay.vue
+++ frontend/src/components/views/board/BoardDisplay.vue
@@ -42,7 +42,7 @@ export default Vue.extend({
     data: () => ({
         tab: null,
         items: [
-          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
+          'Home', 'Popular', 'MyPage', 'Settings',
         ],
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     })
