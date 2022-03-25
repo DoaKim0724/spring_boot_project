71f3b70011da8f09fef6d80ede2872ca38eb26ca
kimdoa <aod1409@naver.com>
2022-03-25 16:45:51 +0900

    list 추가

--- frontend/src/components/views/board/BoardDisplay.vue
+++ frontend/src/components/views/board/BoardDisplay.vue
@@ -33,6 +33,20 @@
             </v-card>
         </v-tab-item>
         </v-tabs-items>
+
+        <v-expansion-panels>
+    <v-expansion-panel
+      v-for="(item,i) in 5"
+      :key="i"
+    >
+      <v-expansion-panel-header>
+        Item
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
   </v-card>
 </template>
 
@@ -51,7 +65,7 @@ export default Vue.extend({
 <style>
 /* Helper classes */
 .basil {
-  background-color: #FFFBE6 !important;
+  background-color: #fcfaf1 !important;
 }
 .basil--text {
   color: #356859 !important;
