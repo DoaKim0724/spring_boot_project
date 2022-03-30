780687044f0e262ec47738f792f4e91a23b61b77
DoaKim0724 <aod1409@naver.com>
2022-03-30 11:28:08 +0900

    port번호 변경

--- frontend/src/components/views/board/BoardDisplay.vue
+++ frontend/src/components/views/board/BoardDisplay.vue
@@ -1,80 +1,57 @@
 <template>
     <v-card color="basil">
         <v-card-title class="text-center justify-center py-6">
-        <h1 class="font-weight-bold text-h2 basil--text">
-            : Life recorD
-        </h1>
+            <h1 class="font-weight-bold text-h2 basil--text">
+                : Life recorD
+            </h1>
         </v-card-title>
 
-        <v-tabs
-        v-model="tab"
-        background-color="transparent"
-        color="basil"
-        grow
-        >
-        <v-tab
-            v-for="item in items"
-            :key="item"
-        >
-            {{ item }}
-        </v-tab>
+        <v-tabs v-model="tab" background-color="transparent" color="basil" grow="grow">
+            <v-tab v-for="item in items" :key="item">
+                {{ item }}
+            </v-tab>
         </v-tabs>
 
         <v-tabs-items v-model="tab">
-        <v-tab-item
-            v-for="item in items"
-            :key="item"
-        >
-            <v-card
-            color="basil"
-            flat
-            >
-            <v-card-text>{{ text }}</v-card-text>
-            </v-card>
-
-            <v-expansion-panels>
-                <v-expansion-panel
-                v-for="(item,i) in 5"
-                :key="i"
-                >
-                <v-expansion-panel-header>
-                    Item
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </v-expansion-panel-content>
-                </v-expansion-panel>
-            </v-expansion-panels>
-
-
-
-
-
-        </v-tab-item>
+            <v-tab-item v-for="item in items" :key="item">
+                <v-card color="basil" flat="flat">
+                    <v-card-text>{{ text }}</v-card-text>
+                </v-card>
+                <!-- <v-expansion-panels>
+                    <v-expansion-panel v-for="(item,i) in 5" :key="i">
+                        <v-expansion-panel-header>
+                            Item
+                        </v-expansion-panel-header>
+                        <v-expansion-panel-content>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </v-expansion-panels> -->
+            </v-tab-item>
         </v-tabs-items>
-
-      
-  </v-card>
+    </v-card>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-    data: () => ({
-        tab: null,
-        items: [
-          'Home', 'Popular', 'MyPage', 'Settings',
-        ],
-        text: '곧 준비할 예정입니다.',
+    import Vue from 'vue'
+    export default Vue.extend({
+        data: () => ({
+            tab: null,
+            items: [
+                'Home', 'MyPage', 'Settings'
+            ],
+            text: '곧 준비할 예정입니다.'
+        })
     })
-})
 </script>
 <style>
-/* Helper classes */
-.basil {
-  background-color: #fcfaf1 !important;
-}
-.basil--text {
-  color: #356859 !important;
-}
+    /* Helper classes */
+    .basil {
+        background-color: #fcfaf1 !important;
+    }
+    .basil--text {
+        color: #356859 !important;
+    }
 </style>
